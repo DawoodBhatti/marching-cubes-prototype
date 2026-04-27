1d445863131f2375caae7868a10d6c305c0e425d
Ali B <d.a.bhatti@hotmail.com>
2026-04-27 10:47:27 +0100

    tweaks

--- marching-cubes-prototype/main.gd
+++ marching-cubes-prototype/main.gd
@@ -1,13 +1,18 @@
 extends Node
 
+@onready var debug : Node = $Debug
+
 func _ready() -> void:
 
 	#print all nodes in scene
 	print()
-	print_all_nodes(get_tree().root)
+	debug.print_all_nodes(get_tree().root)
 	print()
 
 	#place sphere at global origin
-	spawn_debug_sphere()
+	debug.spawn_debug_sphere()
 
 
+	#SDF debug
+	#debug.visualise_SDF_sphere(Color(0.876, 0.361, 0.207, 1.0))
+	debug.visualise_SDF_cube(Color(0.047, 0.498, 0.667, 1.0))
